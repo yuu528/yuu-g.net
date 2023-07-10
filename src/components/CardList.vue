463bf52964a965fd87e29de415a1e57048886268
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-07-10 17:06:50 +0900

    Make CardList.vue as a component

---
+++ src/components/CardList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <div class="text-h4">{{ title }}</div>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12" sm="6" md="4" lg="3" xl="3" xxl="2"
+        v-for="content in list" :key="content.title"
+      >
+        <v-card
+          :title="content.title"
+          :text="content.text"
+          :prepend-icon="content.icon"
+        >
+          <v-card-actions>
+            <v-btn :href="content.href">{{ content.link }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+    'list'
+  ]
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,32 @@
 <template>
-  <v-container fluid>
-    <v-row justify="center">
-      <div class="text-h2" align-content="center">Contents</div>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="6" md="4" lg="3" xl="3" xxl="2" v-for="content in contents" :key="content.title">
-        <v-card
-          :title="content.title"
-          :text="content.text"
-        >
-          <v-card-actions>
-            <v-btn :href="content.href">{{ content.link }}</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <div class="text-h2" align-content="center">Links</div>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="6" md="4" lg="3" xl="3" xxl="2" v-for="link in links" :key="link.title">
-        <v-card
-          :title="link.title"
-          :text="link.text"
-          :prepend-icon="link.icon"
-        >
-          <v-card-actions>
-            <v-btn :href="link.href">{{ link.link }}</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <CardList title="コンテンツ" :list="contents" />
+  <CardList title="リンク" :list="links" />
 </template>
 
+<script setup>
+import CardList from '@/components/CardList.vue'
+</script>
+
 <script>
-  export default {
-    data: () => ({
-      contents: [
-        {
-          title: 'test',
-          text: 'description',
-          link: 'link',
-          href: 'https://example.com'
-        }
-      ],
-      links: [
-        {
-          title: 'Twitter',
-          text: '',
-          link: 'Visit',
-          href: 'https://twitter.com/you_yuu528'
-        }
-      ]
-    })
-  }
+export default {
+  data: () => ({
+    contents: [
+      {
+        title: 'test',
+        text: 'description',
+        link: 'link',
+        href: 'https://example.com'
+      }
+    ],
+    links: [
+      {
+        icon: 'mdi-twitter',
+        title: 'Twitter',
+        text: '',
+        link: 'Visit',
+        href: 'https://twitter.com/you_yuu528'
+      }
+    ]
+  })
+}
 </script>
