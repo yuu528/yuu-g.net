c03ef1812e09191ffbba7dffe97d4171a8ebae55
yuu528 <yuuta.kawaguchi28@gmail.com>
2024-05-07 11:08:04 +0900

    Improve App bar behaviour

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <v-app>
     <v-app-bar color="primary" flat>
-      <v-app-bar-title>Yuu-G.net</v-app-bar-title>
+      <v-app-bar-title v-if="inTabPage">Yuu-G.net</v-app-bar-title>
+      <v-app-bar-title v-else>{{ $router.currentRoute.value.meta.title }}</v-app-bar-title>
 
-      <template v-slot:extension>
+      <template v-slot:prepend v-if="!inTabPage">
+        <v-app-bar-nav-icon
+          v-if="router.currentRoute.value.name === 'NotFound'"
+          to="/"
+          icon="mdi-home"
+        ></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          v-else
+          @click="router.back()"
+          icon="mdi-arrow-left"
+        ></v-app-bar-nav-icon>
+      </template>
+
+      <template v-slot:extension v-if="inTabPage">
         <v-tabs>
           <v-tab v-for="page in pages" :to="page.to" :key="page.to">
             {{ page.name }}
@@ -28,20 +42,28 @@
   </v-app>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      drawer: null,
-      pages: [
-        {
-          name: 'Home',
-          to: '/'
-        },
-        {
-          name: 'About',
-          to: '/about'
-        }
-      ]
-    })
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const pages = [
+  {
+    name: 'Home',
+    to: '/'
+  },
+  {
+    name: 'About',
+    to: '/about'
   }
+];
+
+console.log(router.currentRoute);
+
+const inTabPage = computed(() =>
+  pages.some(page =>
+    page.to === router.currentRoute.value.path
+  )
+)
 </script>
