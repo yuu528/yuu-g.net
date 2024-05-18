fb5b1a8231dcc5fd6e7feafb28a00a51a69ec5c3
Yuu528 <yuu528mail@gmail.com>
2024-05-18 13:58:09 +0900

    Fix width of index page

--- src/components/index/Home.vue
+++ src/components/index/Home.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-sheet class="mt-6 mx-4">
-    <div class="text-h4">コンテンツ</div>
-    <v-sheet class="ml-4">
-      <CardList title="Apps" :list="Contents.apps" />
-      <CardList title="WebApps" :list="Contents.webApps" />
-      <CardList title="UserJS" :list="Contents.userJSs" />
-    </v-sheet>
+  <div class="text-h4">コンテンツ</div>
+  <v-sheet class="ml-4">
+    <CardList title="Apps" :list="Contents.apps" />
+    <CardList title="WebApps" :list="Contents.webApps" />
+    <CardList title="UserJS" :list="Contents.userJSs" />
+  </v-sheet>
 
-    <div class="text-h4">リンク</div>
-    <v-sheet class="ml-4">
-      <CardList title="" :list="Contents.links" />
-    </v-sheet>
+  <div class="text-h4">リンク</div>
+  <v-sheet class="ml-4">
+    <CardList title="" :list="Contents.links" />
   </v-sheet>
 </template>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <Home />
+  <v-container>
+    <Home />
+  </v-container>
 </template>
