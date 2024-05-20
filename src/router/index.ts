2a5e131d148ba4ca7e9fdd30f1a53231a5c3580f
yuu528 <yuuta.kawaguchi28@gmail.com>
2024-05-20 13:51:28 +0900

    Update scrollBehavior of vue-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,9 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition ?? { top: 0 }
+  }
 })
 
 const TITLE_SUFFIX = 'Yuu-G.net'
